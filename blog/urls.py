628ccb7ac5d84c9afbf91e3800ffdf7f2a98376d
obabiy <alex.babiy.sq@gmail.com>
2020-05-29 00:17:35 +0300

    Added page with posts, routing on it. Added posts to the db.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', posts_list)
+    path('', posts_list, name='posts_list_url')   #Задаємо ім'я посиланню і потім звертаємося по ньому (Щоб при зміні url не довелося міняти всі path)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 def posts_list(request):
-    n = ["Alex","Oleg","Maks","Olia"]
-    return render(request, 'blog/index.html', context={'names': n})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
