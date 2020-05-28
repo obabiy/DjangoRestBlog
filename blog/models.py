21236dd4f6b8275f6988550d67fe57264528d6ac
obabiy <alex.babiy.sq@gmail.com>
2020-05-28 23:12:32 +0300

    Provide connection with PostgreSql data base

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+# Додаємо клас(таблицю) в базу данних
+class Post(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    body = models.TextField(blank=True, db_index=True)
+    date_pub = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):                               #вказуєм форматування данних які будуть виводитись
+        return '{}'.format(self.title)
