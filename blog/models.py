b4e2c711a9dc13852231d006698118e4446bb6df
obabiy <alex.babiy.sq@gmail.com>
2020-05-29 01:20:23 +0300

    Added pages for current post, added flexible routing for it

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,17 @@
 from django.db import models
-
+from django.shortcuts import reverse
 
 # Create your models here.
 
 # Додаємо клас(таблицю) в базу данних
 class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
-    slug = models.SlugField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, unique=True)   #те, що буде заміняти айдішку в url і буде зрозуміле людині
     body = models.TextField(blank=True, db_index=True)
     date_pub = models.DateTimeField(auto_now_add=True)
+
+    def get_absolute_url(self):
+        return reverse('post_detail_url', kwargs={'slug': self.slug})   #додаємо метод, який визначає url з відповідим slug для кожного посту
 
     def __str__(self):                               #вказуєм форматування данних які будуть виводитись
         return '{}'.format(self.title)
--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,7 @@ from .models import Post
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
+
+def post_details(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_details.html', context={'post': post})
