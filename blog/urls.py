75c55ac61eca70251ab22ca826ce412c18be68fa
obabiy <alex.babiy.sq@gmail.com>
2020-05-29 11:52:25 +0300

    Remade view methods with classes View

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', posts_list, name='posts_list_url'),  #Задаємо ім'я посиланню і потім звертаємося по ньому (Щоб при зміні url не довелося міняти всі path)
-    path('post/<str:slug>/', post_details, name='post_detail_url'),
+    path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
     path('tags/', tags_list, name='tags_list_url'),
-    path('tag/<str:slug>', tag_detail, name='tag_detail_url')
+    path('tag/<str:slug>', TagDetail.as_view(), name='tag_detail_url')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django.views.generic import View
+
 from .models import Post, Tag
+from .utils import ObjectDetailMixin
 
 # Create your views here.
 
@@ -8,15 +12,32 @@ def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
 
-def post_details(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_details.html', context={'post': post})
+# def post_details(request, slug):
+#     post = Post.objects.get(slug__iexact=slug)
+#     return render(request, 'blog/post_details.html', context={'post': post})
+
+class PostDetail(View):
+    def get(self, request, slug):
+        # post = Post.objects.get(slug__iexact=slug)         #реалізуємо те саме за допомогою get_object_or_404
+        post = get_object_or_404(Post, slug__iexact=slug)    #Додаємо вивід 404, якщо сторінки не існує. get_object_or_404(try catch)
+        return render(request, 'blog/post_details.html', context={'post': post})
 
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
+# def tag_detail(request, slug):
+#     tag = Tag.objects.get(slug__iexact=slug)
+#     return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+class TagDetail(View):
+    def get(self, request, slug):
+        # tag = Tag.objects.get(slug__iexact=slug)
+        tag = get_object_or_404(Tag, slug__iexact=slug)
+        return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+
+
+
+
 
