8f88749c58b55191439a23a542b227bab4ab7ba9
obabiy <alex.babiy.sq@gmail.com>
2020-05-31 20:29:36 +0300

    Added function to delete existing posts. Remade views PostDelete and TagDelete with one mixin ObjectDeleteMixin

--- blog/views.py
+++ blog/views.py
@@ -50,6 +50,11 @@ class PostUpdate(ObjectUpdateMixin, View):
     model_form = PostForm
     template = 'blog/post_update_form.html'
 
+class PostDelete(ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete_form.html'
+    redirect_url = 'posts_list_url'
+
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
@@ -96,14 +101,18 @@ class TagUpdate(ObjectUpdateMixin, View):
     #         return redirect(new_tag)
     #     return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag':tag})
 
-class TagDelete(View):
-    def get(self, request, slug):
-        tag = Tag.objects.get(slug__iexact = slug)
-        return render(request, 'blog/tag_delete_form.html', context={'tag': tag})
-    def post(self, request, slug):
-        tag = Tag.objects.get(slug__iexact = slug)
-        tag.delete()
-        return redirect(reverse('tags_list_url'))
+class TagDelete(ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete_form.html'
+    redirect_url = 'tags_list_url'
+
+    # def get(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact = slug)
+    #     return render(request, 'blog/tag_delete_form.html', context={'tag': tag})
+    # def post(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact = slug)
+    #     tag.delete()
+    #     return redirect(reverse('tags_list_url'))
 
 
 
