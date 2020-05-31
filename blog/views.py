b0fd86f34484e5b08890e5331a9b07b2829d4dae
obabiy <alex.babiy.sq@gmail.com>
2020-05-31 19:45:29 +0300

    Added function to edit existing posts. Remade views PostUpdate and TagUpdate with one mixin ObjectUpdateMixin

--- blog/views.py
+++ blog/views.py
@@ -43,6 +43,12 @@ class PostCreate(ObjectCreateMixin, View):
     #         return redirect(new_post)
     #     return render(request, 'blog/post_create_form.html', context={'form': bound_form})
 
+
+class PostUpdate(ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'blog/post_update_form.html'
+
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
@@ -68,20 +74,26 @@ class TagCreate(ObjectCreateMixin, View):
     #         return redirect(new_tag)
     #     return render(request, 'blog/tag_create.html', context=({'form': bound_form}))
 
-class TagUpdate(View):
-    def get(self, request, slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        bound_form = TagForm(instance=tag)
-        return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag':tag})
+class TagUpdate(ObjectUpdateMixin, View):
 
-    def post(self, request, slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        bound_form = TagForm(request.POST, instance=tag)
+    model = Tag
+    model_form = TagForm
+    template = 'blog/tag_update_form.html'
 
-        if bound_form.is_valid():
-            new_tag = bound_form.save()
-            return redirect(new_tag)
-        return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag':tag})
+
+    # def get(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     bound_form = TagForm(instance=tag)
+    #     return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag':tag})
+    #
+    # def post(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     bound_form = TagForm(request.POST, instance=tag)                                                     #Робимо шаблон (Mixin) для однакових форм редагування постів і тегів
+    #
+    #     if bound_form.is_valid():
+    #         new_tag = bound_form.save()
+    #         return redirect(new_tag)
+    #     return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag':tag})
 
 
 
